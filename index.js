aab6cda72d51526d37dddd76c2bb4f2b6e8013a7
Riyadulislam <riyadulislambd02@gmail.com>
2022-10-27 23:22:50 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const port = process.env.PORT || 7000;
+
+
+const details=require('./data/coursedetails.json')
+app.use(cors())
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+
+app.get('/course', (req, res) => {
+        res.send(details)
+    })
+app.get('/course/:id',(req,res)=>{
+    const id=req.params.id;
+    console.log(id)
+    const selectedNews=details.find(n=>n.id==id);
+     res.send(selectedNews)
+})
+
+
+
+app.listen(port, () => {
+        console.log(`Example app listening on port ${port}`)
+      })
+      
+
+
